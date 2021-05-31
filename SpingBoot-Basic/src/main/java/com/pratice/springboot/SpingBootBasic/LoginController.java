79984d62ccc9a8801982eac439782ed2b44e3489
venkatesh <venkatesh@venkateshs-mbp>
2021-05-31 11:04:29 -0500

    Updated Autowired

--- SpingBoot-Basic/src/main/java/com/pratice/springboot/SpingBootBasic/LoginController.java
+++ SpingBoot-Basic/src/main/java/com/pratice/springboot/SpingBootBasic/LoginController.java
@@ -1,20 +1,24 @@
 package com.pratice.springboot.SpingBootBasic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.pratice.springboot.service.LoginValidatorService;
+import com.pratice.springboot.service.LoginService;
 
 @Controller
+@Component
+@ComponentScan("com.pratice.springboot.service")
 public class LoginController {
 	
-	//@Autowired
-	LoginValidatorService loginValidatorService = new LoginValidatorService();
-
+	@Autowired
+	private LoginService loginService;
+	
 	@GetMapping("/login")
 	public String loginMessage(ModelMap model) {
 		return "login";
@@ -22,7 +26,7 @@ public class LoginController {
 
 	@PostMapping("/login")
 	public String loginMessage(@RequestParam String name,@RequestParam String password, ModelMap model) {
-		boolean isValidate = loginValidatorService.validate(name, password);
+		boolean isValidate = loginService.validate(name, password);
 		
 		if(!isValidate) {
 			model.put("errorMessage", "InvalidCreds!!");
--- SpingBoot-Basic/src/main/java/com/pratice/springboot/service/LoginService.java
+++ SpingBoot-Basic/src/main/java/com/pratice/springboot/service/LoginService.java
@@ -1,11 +1,9 @@
 package com.pratice.springboot.service;
 
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Component;
 
 @Component
-public class LoginValidatorService {
+public class LoginService {
 
 	public boolean validate(String userid, String password) {
 		return userid.equalsIgnoreCase("venkat") && password.equalsIgnoreCase("password");
