1436c93f5a409ad68c7f734b83c703f516d7dab0
venkatesh <venkatesh@venkateshs-mbp>
2021-06-06 16:47:39 -0500

    Prop changes

--- SpingBoot-Advance/src/main/java/com/pratice/springboot/controller/WelcomeController.java
+++ SpingBoot-Advance/src/main/java/com/pratice/springboot/controller/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.pratice.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +10,18 @@ public class WelcomeController {
 
 	@Autowired
 	private WelcomeService welcomeService;
+	
+
 
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome" + "		" + welcomeService.retireWelcomeMessage();
+
+	}
+	
+	@GetMapping("/welcomefromProp")
+	public String welcomefromProp() {
+		return "Welcome" + "		" + welcomeService.retireWelcomeMessageProp();
 
 	}
 
--- SpingBoot-Advance/src/main/java/com/pratice/springboot/controller/WelcomeService.java
+++ SpingBoot-Advance/src/main/java/com/pratice/springboot/controller/WelcomeService.java
@@ -1,11 +1,21 @@
 package com.pratice.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class WelcomeService {
+	@Value("${welcome.message}")
+	private String welcomeMsg;
+	
 	public String retireWelcomeMessage() {
 		return "To SpringBoot Prep";
+
+	}
+	
+	public String retireWelcomeMessageProp() {
+		return welcomeMsg;
 
 	}
 }
