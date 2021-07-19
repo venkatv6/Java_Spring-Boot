b11026b6d6dfbf25997804f6fbad5df09e9a13c4
venkatesh <venkatesh@venkateshs-mbp>
2021-07-19 17:30:57 -0500

    UserRepo

--- SpingBoot-Advance/src/main/java/com/in28minutes/springboot/jpa/UserRepository.java
+++ SpingBoot-Advance/src/main/java/com/in28minutes/springboot/jpa/UserRepository.java
@@ -3,8 +3,11 @@ package com.in28minutes.springboot.jpa;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface UserRepository extends CrudRepository<User, Long>{
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long>{
 
 	List<User> findByRole(String role);
 }
